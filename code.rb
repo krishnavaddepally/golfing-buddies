292ad51dfd98b761896a27b3bf95ae59bc539997
krishnakeerthi <krishnakeerthi675@gmail.com>
2019-02-20 08:34:11 -0500

    Initial commit

---
+++ code.rb
@@ -0,0 +1,10 @@
+golf_contacts = {
+  jose: 'workerBee@example.com',
+  samantha: 'golfCartRacer@example.com',
+  talia: 'pro_golfer89@example.com',
+  mike: 'alwaysAtTheBeach@example.com',
+  olivia: 'didYouSeeWhereThatWent@example.com',
+  joan: 'bestShortGameEver@example.com'
+}
+
+puts [golf_contacts[:samantha], golf_contacts[:talia], golf_contacts[:olivia]]*","
